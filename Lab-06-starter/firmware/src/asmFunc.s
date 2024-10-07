5cbcdc825fdaba5479b6fa2c14df5ce4e6b131b2
nln-anderson <noln.anderson@gmail.com>
2024-10-07 09:32:08 -0700

    finished

--- Lab-06-starter/firmware/src/asmFunc.s
+++ Lab-06-starter/firmware/src/asmFunc.s
@@ -65,8 +65,63 @@ asmFunc:
      * Use it to test the C test code */
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-
     
+    /* First, we store the values from the registers into memory for dividend and divisor */
+    LDR r2, =dividend     
+    LDR r3, =divisor       
+    STR r0, [r2]           
+    STR r1, [r3]          
+
+    /* Initialize quotient and remainder to 0 */
+    LDR r2, =quotient      
+    LDR r3, =mod           
+    MOV r10, 0           
+    STR r10, [r2]          
+    STR r10, [r3]          
+
+    /* Check for errors: either dividend or divisor being 0 */
+    CMP r0, 0             
+    BEQ error              
+    CMP r1, 0             
+    BEQ error         
+
+    /* Division by subtraction method prep*/
+    MOV r5, 0 @Initialize quotient counter (r5 = 0)
+    
+division_algorithm_loop:
+    CMP r0, r1             @ Compare dividend with divisor
+    BCC store_results      @ If dividend < divisor, go to store_results
+
+    SUBS r0, r0, r1        @ Subtract divisor from dividend
+    ADD r5, r5, 1         @ Increment quotient counter
+    B division_algorithm_loop
+
+store_results:
+    /* Store the final results */
+    LDR r2, =quotient      @ Get address of quotient
+    STR r5, [r2]           @ Store quotient in memory
+    LDR r3, =mod           @ Get address of mod (remainder)
+    STR r0, [r3]           @ Store remaining dividend as remainder
+
+    /* Document no error */
+    LDR r6, =we_have_a_problem 
+    MOV r10, 0          
+    STR r10, [r6]
+    
+    /* Store address of quotient in r0 */
+    LDR r0, =quotient
+    
+    B done
+
+error:
+    /* Handle the case where dividend or divisor is 0 */
+    LDR r6, =we_have_a_problem @ Get address of we_have_a_problem
+    MOV r10, 1            @ Set we_have_a_problem to 1 (error)
+    STR r10, [r6]          @ Store 1 in we_have_a_problem
+
+    /* Return the address of quotient in r0 */
+    LDR r0, =quotient      @ Load address of quotient into r0
+    B done                 @ Branch to done
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
 
 done:    
